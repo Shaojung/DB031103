4d70b92d67599d2c29904e16e35e4677aa607a73
Shaojung Lu <shaojung@gmail.com>
2017-03-11 13:33:43 +0800

    use v4 fragment

--- app/src/main/java/com/example/teacher/db031103/MainActivity.java
+++ app/src/main/java/com/example/teacher/db031103/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.teacher.db031103;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv3;
@@ -19,14 +21,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void click(View v)
     {
-        android.app.FragmentManager fm = getFragmentManager();
-        android.app.FragmentTransaction ft = fm.beginTransaction();
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction ft = fm.beginTransaction();
         switch (v.getId())
         {
             case R.id.lotto:
                 ft.add(R.id.containerLayout, new LottoFragment(), "f_lotto");
                 break;
             case R.id.get:
+                List<android.support.v4.app.Fragment> frags = getSupportFragmentManager().getFragments();
                 Fragment f = fm.findFragmentByTag("f_lotto");
                 TextView tv = (TextView) f.getView().findViewById(R.id.textView);
                 String n = tv.getText().toString();
