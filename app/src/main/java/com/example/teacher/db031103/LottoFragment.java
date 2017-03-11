635744b5d2d3573986fdb4fd57be5537f347913c
Shaojung Lu <shaojung@gmail.com>
2017-03-11 11:59:41 +0800

    put random number in it.

--- app/src/main/java/com/example/teacher/db031103/LottoFragment.java
+++ app/src/main/java/com/example/teacher/db031103/LottoFragment.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +24,11 @@ public class LottoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lotto, container, false);
+        View v = inflater.inflate(R.layout.fragment_lotto, container, false);
+        TextView tv = (TextView) v.findViewById(R.id.textView);
+        int r = (int) (39 * Math.random() + 1);
+        tv.setText(String.valueOf(r));
+        return v;
     }
 
 }
